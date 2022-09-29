5bc8a7710e70f9f04e58bca5b8e7743212b9da73
AfanasevKirillGitHub <106386403+AfanasevKirillGitHub@users.noreply.github.com>
2022-09-29 17:00:59 +0300

    task-02 ccomplete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.map((element) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = element;
+  list.append(itemEl);
+});
