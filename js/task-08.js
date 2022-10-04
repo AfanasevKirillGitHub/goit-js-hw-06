0853f2ed4d468060e3df80a95b8eef4b406943fc
AfanasevKirillGitHub <106386403+AfanasevKirillGitHub@users.noreply.github.com>
2022-10-04 15:27:46 +0300

    task-08 - 10 complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+
+  const formData = {
+    email: formEl.email.value,
+    password: formEl.password.value,
+  };
+
+  if (formData.email === "" || formData.password === "") {
+    window.alert(
+      "Пожалуйста, заполните все поля и повторите попытку авторизации"
+    );
+  } else {
+    form.reset();
+    console.log(formData);
+  }
+}
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+
+createBtn.addEventListener("click", createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = Number(input.value);
+
+  const newBox = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+
+    box.style.width = 30 + 10 * i + "px";
+    box.style.height = 30 + 10 * i + "px";
+    box.style.marginTop = "5px";
+    box.style.background = getRandomHexColor();
+
+    newBox.push(box);
+  }
+
+  boxes.style.display = "flex";
+  boxes.style.gap = "5px";
+  boxes.append(...newBox);
+}
+
+function destroyBoxes() {
+  const children = document.querySelectorAll("#boxes > div");
+  for (const child of children) {
+    child.remove();
+    input.value = "";
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
